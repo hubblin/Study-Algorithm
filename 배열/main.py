2dde0e4ae234755dff87ecb0d9cbf716c431fc93
hubblin <chdnjsdkvkxm18@naver.com>
2022-01-13 15:11:39 +0900

    (https://programmers.co.kr/learn/courses/30/lessons/77484) 로또 알고리즘

---
+++ 배열/main.py
@@ -0,0 +1,25 @@
+def check_score(n):
+    print(n)
+    for i in range(1, 7):
+        if i == n:
+            return 7 - i
+    return 6
+
+
+def solution(lottos, win_nums):
+    answer = []
+
+    win = len(set(lottos).intersection(win_nums))
+    don_know = lottos.count(0)
+    max_win = win + don_know
+    min_win = win
+
+    answer.append(check_score(max_win))
+    answer.append(check_score(min_win))
+    return answer
+
+
+lottos = [0, 0, 0, 0, 0, 0]
+win_nums = [38, 19, 20, 40, 15, 25]
+answer = solution(lottos, win_nums)
+print(answer)
